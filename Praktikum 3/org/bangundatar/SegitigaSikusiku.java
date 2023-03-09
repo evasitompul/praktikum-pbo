9cc1ea224290ede64234c7ccb10afb02eea0b1b9
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-03-10 01:04:54 +0700

    Add files via upload

---
+++ Praktikum 3/org/bangundatar/SegitigaSikusiku.java
@@ -0,0 +1,30 @@
+/**
+* Nama				: Eva Sitompul
+* NIM				: 24060119120035
+* Lab				: B2
+* Nama File			: SegitigaSikusiku.java
+* Tanggal Pembuatan	: 08/03/2023
+* Deskripsi			: Representasi dari objek persegi panjang, turunan kelas poligon 
+*/
+
+package org.bangundatar;
+
+import org.poligon.Poligon; //mengimport file sesuai dengan urutan direktori dari paling luar ke dalam
+
+public class SegitigaSikusiku extends Poligon{
+	private double alas,tinggi;
+	
+	public SegitigaSikusiku(double alas,double tinggi,int jumlahSisi){
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return 0.5 * (alas * tinggi);
+	}
+	
+	public void printInfo(){
+		System.out.println("Bangun Segitiga Sikusiku bersisi "+this.getJumlahSisi());
+	}
+}
