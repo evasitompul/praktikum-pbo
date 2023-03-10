5cfec56634a2464295c30da46f2935d35ac75585
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-03-10 19:51:44 +0700

    Praktikum 3

--- Praktikum 3/org/bangundatar/Segitiga.java
+++ Praktikum 3/org/bangundatar/Segitiga.java
@@ -1,15 +1,15 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: Segitiga.java
-* Tanggal Pembuatan		: 08/03/2023
-* Deskripsi			: Representasi dari objek segitiga, turunan kelas poligon 
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: Segitiga.java
+* Tanggal Pembuatan	: 08/03/2023
+* Deskripsi		: Representasi dari objek segitiga, turunan kelas poligon 
 */
 
 package org.bangundatar;
 
-import org.poligon.Poligon; //mengimport file sesuai dengan urutan direktori
+import org.poligon.Poligon;
 
 public class Segitiga extends Poligon{
 	private double alas,tinggi;
