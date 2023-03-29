eb631efd9b24ce2ac503e00693c1e36839c32a1f
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-03-29 14:59:32 +0700

    Praktikum 5

--- Praktikum 5/IArea.java
+++ Praktikum 5/IArea.java
@@ -1,13 +1,13 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: IArea.java
-* Tanggal Pembuatan	: 27/02/2023
-* Deskripsi			: Interface yang mendefinisikan kontrak
-					bahwa setiap kelas yang membuat implementasi
-					dari interface ini harus memiliki cara
-					untuk menghitung luas.
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: IArea.java
+* Tanggal Pembuatan	: 27/03/2023
+* Deskripsi		: Interface yang mendefinisikan kontrak
+			bahwa setiap kelas yang membuat implementasi
+			dari interface ini harus memiliki cara
+			untuk menghitung luas.
 */
 public interface IArea{
 	public double hitungLuas();
