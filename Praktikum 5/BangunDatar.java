4dd6d203fe784eea8401b7e9ebae231dec792080
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-03-29 15:00:00 +0700

    Praktikum 5

--- Praktikum 5/BangunDatar.java
+++ Praktikum 5/BangunDatar.java
@@ -1,10 +1,10 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: BangunDatar.java
-* Tanggal Pembuatan		: 27/03/2023
-* Deskripsi			: Kelas abstak, berisi abstraksi bangun datar
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: BangunDatar.java
+* Tanggal Pembuatan	: 27/03/2023
+* Deskripsi		: Kelas abstak, berisi abstraksi bangun datar
 */
 
 public abstract class BangunDatar{
