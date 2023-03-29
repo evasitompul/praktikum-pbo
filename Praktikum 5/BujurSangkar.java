46a8acedc2eaf7a47675f32e8d8e64a398ecc2fe
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-03-29 15:00:29 +0700

    Praktikum 5

--- Praktikum 5/BujurSangkar.java
+++ Praktikum 5/BujurSangkar.java
@@ -1,10 +1,10 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: BujurSangkar.java
-* Tanggal Pembuatan		: 27/03/2023
-* Deskripsi			: Kelas yang membuat implementasi abstrak pada bangun datar
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: BujurSangkar.java
+* Tanggal Pembuatan	: 27/03/2023
+* Deskripsi		: Kelas yang membuat implementasi abstrak pada bangun datar
 */
 
 public class BujurSangkar extends BangunDatar{
