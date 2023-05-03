2c5ab65929b3a3da3f6f8bd2de8f3c2c6993a569
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-05-04 06:55:49 +0700

    Praktikum 7

---
+++ Praktikum 7/Inclusion_1/Sewa.java
@@ -0,0 +1,21 @@
+/**
+* Nama				: Eva Sitompul
+* NIM				: 24060119120035
+* Lab				: B2
+* Nama File			: Sewa.java
+* Tanggal Pembuatan	: 03/05/2023
+* Deskripsi			: Kelas yang berisi program untuk kelas Sewa.
+*/
+
+import java.util.Scanner;
+
+public class Sewa{
+	public static void main (String[] args){
+		Vehicle kendaraan = new Vehicle();
+		Vehicle mobil = new Car();
+		Vehicle bis = new Bus();
+		kendaraan.calRent(50, 1000);
+		mobil.calRent(50, 1000);
+		bis.calRent(50, 2000);
+	}
+}
