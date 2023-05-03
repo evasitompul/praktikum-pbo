0b9fdd1ea12d1b8d7133528c2ca891b7d696021c
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-05-03 16:36:38 +0700

    Praktikum 7

--- Praktikum 7/Bus.java
+++ Praktikum 7/Bus.java
@@ -1,10 +1,10 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: Bus.java
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: Bus.java
 * Tanggal Pembuatan	: 03/05/2023
-* Deskripsi			: Kelas yang berisi program untuk kelas Bus.
+* Deskripsi		: Kelas yang berisi program untuk kelas Bus.
 */
 
 import java.util.Scanner;
