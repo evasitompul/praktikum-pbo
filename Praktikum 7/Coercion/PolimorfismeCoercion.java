26db89e385980bdc73b72185566448623d8c4302
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-05-04 07:02:01 +0700

    Praktikum 7

--- Praktikum 7/Coercion/PolimorfismeCoercion.java
+++ Praktikum 7/Coercion/PolimorfismeCoercion.java
@@ -1,21 +1,21 @@
 /**
-* Nama				      : Eva Sitompul
-* NIM				        : 24060119120035
-* Lab				        : B2
-* Nama File			    : PolimorfismeCoercion.java
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: PolimorfismeCoercion.java
 * Tanggal Pembuatan	: 03/05/2023
-* Deskripsi			    : Kelas yang berisi program untuk kelas PolimorfismeCoercion.
+* Deskripsi		: Kelas yang berisi program untuk kelas PolimorfismeCoercion.
 */
 
 public class PolimorfismeCoercion{
 	
-	public static int kuadrat(int bilangan){ //int masuk ke dalam tipe data yang primitif
+	public static int kuadrat(int bilangan){ 
 		return bilangan*bilangan;
 	}
 	
 	public static void main(String[] args){
 		//deklarasi objek integer
-		Integer bilangan = 10; //Integer di sini merupakan sebuah kelas/instans
+		Integer bilangan = 10; 
 		
 		//objek integer 'dipaksa' untuk diubah menjadi primitif
 		int hasilKuadrat = kuadrat(bilangan);
