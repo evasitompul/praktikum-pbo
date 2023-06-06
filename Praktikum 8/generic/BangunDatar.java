f21a49473617eac1901913130bd9c17d5f75bac1
evasitompul <126045368+evasitompul@users.noreply.github.com>
2023-06-06 20:06:44 +0700

    Praktikum 8

--- Praktikum 8/generic/BangunDatar.java
+++ Praktikum 8/generic/BangunDatar.java
@@ -1,10 +1,10 @@
 /**
-* Nama				: Eva Sitompul
-* NIM				: 24060119120035
-* Lab				: B2
-* Nama File			: BangunDatar.java
+* Nama			: Eva Sitompul
+* NIM			: 24060119120035
+* Lab			: B2
+* Nama File		: BangunDatar.java
 * Tanggal Pembuatan	: 17/05/2023
-* Deskripsi			: kelas abstrak untuk bangun datar
+* Deskripsi		: kelas abstrak untuk bangun datar
 */
 public abstract class BangunDatar{
 	public abstract double hitungKeliling();
